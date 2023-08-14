bedeee472f3356a4cfa227150d9e5fd60e10b9b3
RainbowStays <46221436+RainbowStays@users.noreply.github.com>
2023-08-15 00:21:57 +0800

    add video

--- video_process.py
+++ video_process.py
@@ -9,6 +9,10 @@ def save_frames(video_path, output_folder, image_size):
     # 打开视频文件
     video_capture = cv2.VideoCapture(video_path)
 
+    if not video_capture.isOpened():
+        print("Failed to open")
+        exit()
+
     frame_count = 0
     unit_count = 0
     train_frame_count = 0
@@ -25,7 +29,7 @@ def save_frames(video_path, output_folder, image_size):
 
         # 每50帧一个单位
         if frame_count % 50 == 0:
-            unit_folder_name = datetime.now().strftime("%Y%m%d%H%M%S")
+            unit_folder_name = "unit" + f"{unit_count:05}"
             unit_folder_path = os.path.join(output_folder, unit_folder_name)
             os.makedirs(unit_folder_path, exist_ok=True)
             unit_count += 1
@@ -43,7 +47,7 @@ def save_frames(video_path, output_folder, image_size):
             test_frame_count += 1
 
         # 保存图像
-        image_name = f"{sub_frame_count:04}.jpg"
+        image_name = f"frame{sub_frame_count:05}.jpg"
         output_path = os.path.join(unit_folder_path, sub_folder, image_name)
         cv2.imwrite(output_path, frame)
 
@@ -53,9 +57,9 @@ def save_frames(video_path, output_folder, image_size):
 
 
 # 输入视频文件路径和输出文件夹路径
-video_path = "../input.mp4"
-output_folder = "../output"
-image_size = (640, 480)
+video_path = "input.mp4"
+output_folder = "videooutput"
+image_size = (640, 360)
 
 # 处理视频并保存图像
 save_frames(video_path, output_folder, image_size)
